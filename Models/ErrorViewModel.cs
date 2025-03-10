191058c5385de1eedcc69655e43e6006dc551e4d
VoorTex20 <oijoaovini20@gmail.com>
2025-03-10 08:52:37 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pc_Express.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
