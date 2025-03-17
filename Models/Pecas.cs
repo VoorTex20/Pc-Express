d2bff57817c5d7f573a1418ef99ff7a2d0ad0324
VoorTex20 <oijoaovini20@gmail.com>
2025-03-17 11:28:51 -0300

    .

--- Models/Pecas.cs
+++ Models/Pecas.cs
@@ -1,6 +1,18 @@
 ﻿namespace Pc_Express.Models
 {
-    public class Peças
+    public class Pecas
     {
+        public int PecasID { get; set; }
+        public string nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string imagemUrl { get; set; }
+        public string imagemThumbnailUrl { get; set; }
+        public bool Ispecapreferida { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int categoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
     }
 }
